970f546f3d295b9eb7e1e35491aaa18e5fd53bfa
Tushar Kant Mohapatra <kanttushar47@gmail.com>
2025-02-14 15:01:39 +0530

    Create Space.java

---
+++ Space.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Space{
+	public static void main(String args[]){
+
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Size: ");
+		int a=scan.nextInt();
+		int[] arr= new int[a];
+
+		System.out.print("Space COMPLEXITY: "+a+" \n");
+
+		}
+	}
