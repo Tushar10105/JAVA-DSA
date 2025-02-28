7a05ec332e2ebfb9682513d7bd2eb1dd1da9b126
Tushar Kant Mohapatra <kanttushar47@gmail.com>
2025-02-28 23:51:17 +0530

    Update ArrInsrtAtEnd.java

--- ArrInsrtAtEnd.java
+++ ArrInsrtAtEnd.java
@@ -4,7 +4,8 @@ public static void main(String args[]){
 	Scanner scan=new Scanner(System.in);
 	System.out.print("Size: ");
 	int size=scan.nextInt();
-	int[] arr=new int[size];
+	int[] arr=new int[size+1];
+
 	if (size <= 0) {
 		System.out.println("Invalid size! Must be greater than 0.");
 		return;
@@ -15,20 +16,15 @@ public static void main(String args[]){
 		arr[i]=scan.nextInt();
 		}
 
-		int[] newarr=new int[arr.length+1];
 		System.out.print("NEW Element: ");
 		int element=scan.nextInt();
-		newarr[newarr.length-1]=element;
-
-		for(int i=0;i<arr.length;i++){
-			newarr[i]=arr[i];
-			}
+		arr[arr.length-1]=element;
 
 		System.out.print("After INSERTION at the END:\n");
 
-		for(int i:newarr){
+		for(int i:arr){
 			System.out.print(i+" | ");
 			}
-		System.out.print("\n");
+		System.out.println();
 	}
 }
