aa46bf88f77f5efeaee0369be0150781ea8a73f0
Tushar Kant Mohapatra <kanttushar47@gmail.com>
2025-03-01 00:36:16 +0530

    Update ArrDelAtEnd.java

--- ArrDelAtEnd.java
+++ ArrDelAtEnd.java
@@ -1,30 +1,28 @@
 import java.util.Scanner;
-public class ArrDelAtEnd{
-	public static void main(String args[]){
-		Scanner scan=new Scanner(System.in);
-		System.out.print("Size: ");
-		int size=scan.nextInt();
 
-		if (size <= 0) {
-			System.out.println("Invalid size!");
-			return;
-	        }
+public class ArrDelAtEnd {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Size: ");
+        int size=scan.nextInt();
 
-		int[] arr= new int[size];
-		System.out.print("ELEMENTS:\n");
-		for(int i=0;i<arr.length;i++){
-			arr[i]=scan.nextInt();
-			}
+        if (size<=0) {
+            System.out.println("Invalid size!");
+            return;
+        }
 
-			int[] newarr=new int[arr.length];
+        int[] arr=new int[size];
+        System.out.print("ELEMENTS:\n");
+        for (int i=0;i<size;i++) {
+            arr[i]=scan.nextInt();
+        }
 
-			for(int i:newarr){
-				newarr[i]=arr.length-1;
-				}
-				System.out.print("After DELETION:\n");
-				for(int i=0;i<newarr.length;i++){
-					System.out.print(i+" | ");
-					}
-			//System.out.print(newarr.length-1+"\n");
-		}
-	}
+        arr[size-1]=0;
+
+        System.out.print("After DELETION:\n");
+        for(int i=0;i<size-1;i++){
+            System.out.print(arr[i]+" | ");
+        }
+        System.out.println();
+    }
+}
